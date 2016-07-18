68cd977ad1826e776778ba353f7e620feb703d9b
gary <ggrumbley@ucsd.edu>
2016-07-18 14:23:55 -0700

    Word count app complete

--- app.py
+++ app.py
@@ -13,16 +13,19 @@ from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
 
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 q = Queue(connection=conn)
 
 from models import *
 
-# count the words and save to db
+
 def count_and_save_words(url):
+
     errors = []
 
     try:
@@ -31,11 +34,11 @@ def count_and_save_words(url):
         errors.append(
             "Unable to get URL. Please make sure it's valid and try again."
         )
-        return {'error': errors}
+        return {"error": errors}
 
     # text processing
-    raw = BeautifulSoup(r.text).get_text()
-    nltk.data.path.append('./nltk_data/') # set the path
+    raw = BeautifulSoup(r.text, 'html.parser').get_text()
+    nltk.data.path.append('./nltk_data/')  # set the path
     tokens = nltk.word_tokenize(raw)
     text = nltk.Text(tokens)
 
@@ -48,7 +51,7 @@ def count_and_save_words(url):
     no_stop_words = [w for w in raw_words if w.lower() not in stops]
     no_stop_words_count = Counter(no_stop_words)
 
-    # save results
+    # save the results
     try:
         result = Result(
             url=url,
@@ -59,26 +62,31 @@ def count_and_save_words(url):
         db.session.commit()
         return result.id
     except:
-        errors.append('Unable to add item to database.')
-        return {'error': errors}
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    results = {}
-    if request.method == 'POST':
-        # get url that the user has entered
-        url = request.form['url']
-        if 'http://' not in url[:7]:
-            url = 'http://' + url
-        job = q.enqueue_call(
-            func=count_and_save_words, args=(url,), result_ttl=5000
-        )
-        print(job.get_id())
+    return render_template('index.html')
 
-    return render_template('index.html', results=results)
 
-@app.route('/results/<job_key>', methods=['GET'])
+@app.route('/start', methods=['POST'])
+def get_counts():
+    # get url
+    data = json.loads(request.data.decode())
+    url = data["url"]
+    if 'http://' not in url[:7]:
+        url = 'http://' + url
+    # start job
+    job = q.enqueue_call(
+        func=count_and_save_words, args=(url,), result_ttl=5000
+    )
+    # return created job id
+    return job.get_id()
+
+
+@app.route("/results/<job_key>", methods=['GET'])
 def get_results(job_key):
 
     job = Job.fetch(job_key, connection=conn)
@@ -92,7 +100,8 @@ def get_results(job_key):
         )[:10]
         return jsonify(results)
     else:
-        return 'Nay!', 202
+        return "Nay!", 202
+
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
---
+++ static/main.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('WordcountApp', [])
+    .controller('WordcountController', WordcountController);
+
+  WordcountController.$inject = ['$scope', '$log', '$http', '$timeout']
+  function WordcountController($scope, $log, $http, $timeout) {
+    $scope.submitButtonText = 'Submit';
+    $scope.loading = false;
+
+    $scope.getResults = function() {
+
+      // get the URL from the input
+      var userInput = $scope.url;
+
+      // fire the API request
+      $http.post('/start', {"url": userInput})
+        .success(function(results) {
+          $log.log(results);
+          getWordCount(results);
+          $scope.wordcounts = null;
+          $scope.loading = true;
+          $scope.submitButtonText = 'Loading...';
+        })
+        .error(function(error) {
+          $log.log(error);
+        });
+    };
+    function getWordCount(jobID) {
+      var timeout = "";
+
+      var poller = function() {
+        // fire another request
+        $http.get('/results/' + jobID)
+          .success(function (data, status, headers, config) {
+            if (status === 202) {
+              $log.log(data, status);
+            } else if (status === 200) {
+              $log.log(data);
+              $scope.loading = false;
+              $scope.submitButtonText = "Submit";
+              $scope.wordcounts = data;
+              $timeout.cancel(timeout);
+              return false;
+            }
+            // continue to call the poller() function every 2 seconds
+            // until the timeout is cancelled
+            timeout = $timeout(poller, 2000);
+          });
+      };
+      poller();
+    }
+  }
+}());
